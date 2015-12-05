2d6585c868f917d327cb72a4c6ccce98f92304c5
Stefan Liebenberg <siga.fredo@gmail.com>
2015-12-05 21:03:49 +0000

    fix js style

--- src/main/javascript/slieb/injector/providers/ProviderFactory.js
+++ src/main/javascript/slieb/injector/providers/ProviderFactory.js
@@ -13,10 +13,10 @@ goog.require('slieb.injector.providers.SingletonProvider');
  * @extends {goog.Disposable}
  * @param {slieb.injector.Injector} injector
  * */
-slieb.injector.providers.ProviderFactory = function(injector) {
-    goog.asserts.assert(injector instanceof slieb.injector.Injector);
-    slieb.injector.providers.ProviderFactory.base(this, 'constructor');
-    this.injector = injector;
+slieb.injector.providers.ProviderFactory = function (injector) {
+  goog.asserts.assert(injector instanceof slieb.injector.Injector);
+  slieb.injector.providers.ProviderFactory.base(this, 'constructor');
+  this.injector = injector;
 };
 goog.inherits(slieb.injector.providers.ProviderFactory, goog.Disposable);
 
@@ -26,20 +26,21 @@ goog.inherits(slieb.injector.providers.ProviderFactory, goog.Disposable);
  * @return {slieb.injector.providers.ConstructorProvider.<T>}
  * @template T
  */
-slieb.injector.providers.ProviderFactory.prototype.createConstructorProvider = function(ctor) {
-    goog.asserts.assert(goog.isFunction(ctor));
-    goog.asserts.assert(ctor.length == 0);
-    var provider = new slieb.injector.providers.ConstructorProvider(ctor);
-    this.registerDisposable(provider);
-    return provider;
+slieb.injector.providers.ProviderFactory.prototype
+  .createConstructorProvider = function (ctor) {
+  goog.asserts.assert(goog.isFunction(ctor));
+  goog.asserts.assert(ctor.length == 0);
+  var provider = new slieb.injector.providers.ConstructorProvider(ctor);
+  this.registerDisposable(provider);
+  return provider;
 };
 /**
  * @param {T} object
  * @return {slieb.injector.providers.FunctionProvider.<T>}
  * @template T
  */
-slieb.injector.providers.ProviderFactory.prototype.createConstantProvider = function(object) {
-    return this.createFunctionProvider(goog.functions.constant(object));
+slieb.injector.providers.ProviderFactory.prototype.createConstantProvider = function (object) {
+  return this.createFunctionProvider(goog.functions.constant(object));
 };
 
 /**
@@ -47,11 +48,11 @@ slieb.injector.providers.ProviderFactory.prototype.createConstantProvider = func
  * @param {function():T|function(slieb.injector.Injector):T} method
  * @return {slieb.injector.providers.FunctionProvider.<T>}
  */
-slieb.injector.providers.ProviderFactory.prototype.createFunctionProvider = function(method) {
-    goog.asserts.assert(goog.isFunction(method));
-    var provider = new slieb.injector.providers.FunctionProvider(this.injector, method);
-    this.registerDisposable(provider);
-    return provider;
+slieb.injector.providers.ProviderFactory.prototype.createFunctionProvider = function (method) {
+  goog.asserts.assert(goog.isFunction(method));
+  var provider = new slieb.injector.providers.FunctionProvider(this.injector, method);
+  this.registerDisposable(provider);
+  return provider;
 };
 
 /**
@@ -59,12 +60,12 @@ slieb.injector.providers.ProviderFactory.prototype.createFunctionProvider = func
  * @return {slieb.injector.providers.SingletonProvider.<T>}
  * @template T
  */
-slieb.injector.providers.ProviderFactory.prototype.createSingletonProvider = function(provider) {
-    goog.asserts.assert(provider instanceof goog.Disposable);
-    var singletonProvider = new slieb.injector.providers.SingletonProvider(provider);
-    this.registerDisposable(singletonProvider);
-    (/** @type {goog.Disposable} */(provider)).registerDisposable(singletonProvider);
-    return singletonProvider;
+slieb.injector.providers.ProviderFactory.prototype.createSingletonProvider = function (provider) {
+  goog.asserts.assert(provider instanceof goog.Disposable);
+  var singletonProvider = new slieb.injector.providers.SingletonProvider(provider);
+  this.registerDisposable(singletonProvider);
+  (/** @type {goog.Disposable} */(provider)).registerDisposable(singletonProvider);
+  return singletonProvider;
 };
 
 /**
@@ -72,12 +73,12 @@ slieb.injector.providers.ProviderFactory.prototype.createSingletonProvider = fun
  * @return {slieb.injector.providers.DisposableSingletonProvider.<T>}
  * @template T
  */
-slieb.injector.providers.ProviderFactory.prototype.createDisposableSingletonProvider = function(provider) {
-    goog.asserts.assert(provider instanceof goog.Disposable);
-    var singletonProvider = new slieb.injector.providers.DisposableSingletonProvider(provider);
-    this.registerDisposable(singletonProvider);
-    (/** @type {goog.Disposable} */(provider)).registerDisposable(singletonProvider);
-    return singletonProvider;
+slieb.injector.providers.ProviderFactory.prototype.createDisposableSingletonProvider = function (provider) {
+  goog.asserts.assert(provider instanceof goog.Disposable);
+  var singletonProvider = new slieb.injector.providers.DisposableSingletonProvider(provider);
+  this.registerDisposable(singletonProvider);
+  (/** @type {goog.Disposable} */(provider)).registerDisposable(singletonProvider);
+  return singletonProvider;
 };
 
 /**
@@ -85,12 +86,12 @@ slieb.injector.providers.ProviderFactory.prototype.createDisposableSingletonProv
  * @return {slieb.injector.providers.DisposingProvider.<T>}
  * @template T
  */
-slieb.injector.providers.ProviderFactory.prototype.createDisposingProvider = function(provider) {
-    goog.asserts.assert(provider instanceof goog.Disposable);
-    var disposingProvider = new slieb.injector.providers.DisposingProvider(this.injector, provider);
-    this.registerDisposable(disposingProvider);
-    (/** @type {goog.Disposable} */(provider)).registerDisposable(disposingProvider);
-    return disposingProvider;
+slieb.injector.providers.ProviderFactory.prototype.createDisposingProvider = function (provider) {
+  goog.asserts.assert(provider instanceof goog.Disposable);
+  var disposingProvider = new slieb.injector.providers.DisposingProvider(this.injector, provider);
+  this.registerDisposable(disposingProvider);
+  (/** @type {goog.Disposable} */(provider)).registerDisposable(disposingProvider);
+  return disposingProvider;
 };
 
 /**
@@ -98,17 +99,17 @@ slieb.injector.providers.ProviderFactory.prototype.createDisposingProvider = fun
  * @param {slieb.injector.Scope} scope
  * @return {slieb.injector.Provider}
  */
-slieb.injector.providers.ProviderFactory.prototype.createProviderForScope = function(provider, scope) {
-    var s = slieb.injector.Scope;
-    switch (scope) {
-        case s.SINGLETON:
-            return this.createSingletonProvider(provider);
-        case s.DISPOSABLE_SINGLETON:
-            return this.createDisposableSingletonProvider(this.createDisposingProvider(provider));
-        case s.DISPOSABLE:
-            return this.createDisposingProvider(provider);
-        case s.CLASS:
-        default:
-            return provider;
-    }
+slieb.injector.providers.ProviderFactory.prototype.createProviderForScope = function (provider, scope) {
+  var s = slieb.injector.Scope;
+  switch (scope) {
+    case s.SINGLETON:
+      return this.createSingletonProvider(provider);
+    case s.DISPOSABLE_SINGLETON:
+      return this.createDisposableSingletonProvider(this.createDisposingProvider(provider));
+    case s.DISPOSABLE:
+      return this.createDisposingProvider(provider);
+    case s.CLASS:
+    default:
+      return provider;
+  }
 };
